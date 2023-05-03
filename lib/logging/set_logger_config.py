0adf9e6ff501b5c30a01dfbc8610482ab38ca6ba
Joe Saddigh <joesaddigh@gmail.com>
2023-05-03 15:20:10 +0100

    TestInputs: Change CropGen to allow APSIM Inputs generated from the algorithm, to be overridden by a CSV file (inputs.csv - Header row ignored). Delete CropGen log file each time the app is ran.

--- lib/logging/set_logger_config.py
+++ lib/logging/set_logger_config.py
@@ -6,6 +6,10 @@ root_directory = os.path.join(os.path.dirname(__file__), "..", "..", "logs")
 if not os.path.exists(root_directory):
     os.makedirs(root_directory)
 
+log_file = os.path.join(root_directory, 'cropgen.log')
+if os.path.exists(log_file):
+    os.remove(log_file)
+
 # Simple script setup the logger using the config ini file.
 logger_config = "logger_config.ini"
 logging_config_path = os.path.join(os.path.dirname(__file__), logger_config)
--- lib/models/cgm/relay_apsim.py
+++ lib/models/cgm/relay_apsim.py
@@ -1,3 +1,9 @@
+import numpy as NumPy
+import logging
+import csv
+import os.path
+
+from os.path import exists
 from lib.models.common.model import Model
 
 #
@@ -11,7 +17,17 @@ class RelayApsim(Model):
     def __init__(self, run_job_request, generated_input_values):
         self.JobID = run_job_request.JobID
         self.Individuals = run_job_request.Individuals
-        self.Inputs = RelayApsim.create_input_values(generated_input_values)
+        inputs_file = RelayApsim._get_inputs_file_path()
+
+        if exists(inputs_file):
+            self.Inputs = RelayApsim.create_input_values_from_file(
+                generated_input_values,
+                inputs_file, 
+                run_job_request.Individuals,
+                run_job_request.get_total_inputs()
+            )
+        else:
+            self.Inputs = RelayApsim.create_input_values(generated_input_values)
 
     #
     # Returns the type name.
@@ -19,6 +35,49 @@ class RelayApsim(Model):
     def get_type_name(self):
         return __class__.__name__
     
+    #
+    #
+    #
+    @staticmethod
+    def _get_inputs_file_path():
+        return os.path.join(os.path.dirname(__file__), "..", "..", "..", "inputs.csv")
+
+    #
+    #
+    #
+    @staticmethod
+    def create_input_values_from_file(
+        generated_input_values,
+        inputs_file, 
+        total_individuals,
+        total_inputs
+    ):
+        input_values = []
+        with open(inputs_file, "r") as f:
+            reader = csv.reader(f, delimiter="\t")
+            for line_number, line in enumerate(reader):
+                if line_number > total_individuals: break
+
+                if line_number == 0:
+                    logging.info('Header line[{}] = {}'.format(line_number, line))
+                else:
+                    input_values.append(RelayApsim.HandleInputFileRow(generated_input_values, line_number, line, total_inputs))
+        return input_values
+
+    #
+    #
+    #
+    @staticmethod
+    def HandleInputFileRow(generated_input_values, line_number, line, total_inputs):
+        logging.info('line[{}] = {}'.format(line_number, line))
+        line_entries = line[0].split(',')
+        values = [line_number - 1]
+        for input_value in range(0, total_inputs):
+            values.append(float(line_entries[input_value]))
+        generated_input_values[line_number-1] = NumPy.array(values[1:])
+
+        return values
+
     #
     # Creates input values, using the generated input values.
     #
