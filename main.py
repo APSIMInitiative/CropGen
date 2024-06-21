8e2ecd52f7a0ab979394765868f4e35128237b47
joesaddigh <joesaddigh@gmail.com>
2024-06-21 16:32:34 +0100

    Tidy up.

--- main.py
+++ main.py
@@ -1,57 +1,21 @@
-#
-# Main application entry point.
-#
-
-# Imports
-import asyncio
 import logging
 
-from lib.logging.logger_config import LoggerConfig
-from lib.server.socket_server import SocketServer
-from lib.server.server_state import ServerState
-from lib.config.crop_gen_config import CropGenConfig
-
-config = CropGenConfig()
-config._parse()
-server_state = ServerState()
-
-# Functions
-def client_connected_cb(client_reader, client_writer):
-    server = SocketServer(config, server_state)
-    server.client_connected_callback(client_reader, client_writer)
-
-def log_app_startup():
-    logging.info("Started CropGen application")
-    logging.info("Service Config: %s", config.to_json(config.PrettyPrintJsonInLogs))
-    
+from lib.server.cropgen_runner import CropGenRunner
 
 # Main entry point
 if __name__ == "__main__":
-
     try:
-        logger_config = LoggerConfig(config)
-        logger_config.setup_logger(True)
+        crop_gen_runner = CropGenRunner()
 
-        log_app_startup()
+        # Run the loop forever until a keyboard event occurs
+        while True:
+            try:
+                crop_gen_runner.run()
+            except KeyboardInterrupt: 
+                # Break out of the loop on a keyboard event
+                logging.info("Keyboard interrupt. Exiting...")
+                break
 
-        loop = asyncio.get_event_loop()
-        server_coro = asyncio.start_server(
-            client_connected_cb,
-            host=config.SocketServerHost,
-            port=config.SocketServerPort
-        )
-
-        server = loop.run_until_complete(server_coro)
-
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt as e:
-            pass
-
-        # Close the server
-        server.close()
-        loop.run_until_complete(server.wait_closed())
-        loop.close()
         logging.info("Closing CropGen application")
     except:
         logging.exception("Exception - CropGen Main Application catch handler")
